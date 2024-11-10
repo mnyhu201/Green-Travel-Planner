dd2ee9e6b78e8e9f33928ba1660727e6886691d9
Xinyang Xu <epiphany0625@outlook.com>
2024-11-09 19:44:39 -0800

    update

--- frontend/src/pages/ItineraryPage.js
+++ frontend/src/pages/ItineraryPage.js
@@ -1,6 +1,14 @@
 // Example HomePage.js
 import React from 'react';
+import ItineraryForm from '../components/ItineraryForm';
 
-const ItineraryPage = () => <div><h1>Itinerary Page</h1></div>;
+const ItineraryPage = () => {
+    return(
+        <div>
+            <h1>Itinerary Page</h1>
+            <ItineraryForm />
+        </div>
+    )
+}
 
 export default ItineraryPage;
